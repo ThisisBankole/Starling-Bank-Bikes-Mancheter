90b0ebbbd159dbdf0bcc5d3ad332fcc6e038050a
thisisbankole <bjamgbadi@gmail.com>
2024-12-07 20:38:15 +0000

    attempt to fix deplomynet issues

--- bbike/app/main.py
+++ bbike/app/main.py
@@ -22,7 +22,7 @@ allowed_origins = [
 #if settings.BACKEND_CORS_ORIGINS:
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
